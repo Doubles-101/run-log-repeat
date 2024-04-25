63af6335ff8e588e2445b8202529780fe1c8fcc4
William Woodard <williamwoodard98@gmail.com>
2024-04-25 16:09:49 -0500

    Added Run post CSS

--- src/components/home/runs/Run.jsx
+++ src/components/home/runs/Run.jsx
@@ -27,22 +27,22 @@ export const Run = ({runObject, setLikeList}) => {
     }, [runDetails])
 
     return (
-        <div className="run-post">
-            <header>
-                <h2>{runObject.date}</h2>
-                <Link className="run-post-profileLink" to={`/profile/${runDetails[0]?.user?.id}`}>
-                    <h2>{runDetails[0]?.user?.username}</h2>
-                </Link>
-            </header>
-            <div>
-                <h3>Distance : {runObject.distance}</h3>
-                <h3>Time : {runObject.time} mins</h3>
-                <h3>Location : {runObject.location}</h3>
-            </div>
-            <footer>
-                <LikeButton runObject={runObject} setLikeCounter={setLikeCounter} likeCounter={likeCounter} setLikeList={setLikeList} />
-                <h3>Number of Likes: {likeCounter}</h3>
-            </footer>
+    <div className="bg-blue-800 text-white p-8 rounded-lg shadow-md m-4">
+        <header>
+            <h2 className="text-xl font-bold m-2">{runObject.date}</h2>
+            <Link className="run-post-profileLink" to={`/profile/${runDetails[0]?.user?.id}`}>
+                <h2 className="text-lg font-semibold mb-2">{runDetails[0]?.user?.username}</h2>
+            </Link>
+        </header>
+        <div>
+            <h3 className="text-lg mb-2">Distance: {runObject.distance}</h3>
+            <h3 className="text-lg mb-2">Time: {runObject.time} mins</h3>
+            <h3 className="text-lg mb-2">Location: {runObject.location}</h3>
         </div>
-    )
+        <footer className="flex items-center justify-between">
+            <LikeButton runObject={runObject} setLikeCounter={setLikeCounter} likeCounter={likeCounter} setLikeList={setLikeList} />
+            <h3 className="text-lg">Number of Likes: {likeCounter}</h3>
+        </footer>
+    </div>
+)
 }
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -12,66 +12,87 @@ export const NavBar = ({currentUser}) => {
 
     return(
         <div className="navbar">
-            <ul className="navbar">
-                <li className="navbar-li text-left" onClick={toggleDropdown}>RUNS
-                {isDropdownOpen && 
-                    <div className="relative">
-                        <ul>
-                            <li className="navbar-li text-black text-left p-2 hover:text-white hover:underline">
-                                <Link className="navbar-link" to="/home">
-                                    All Runs
-                                </Link>
-                            </li>
-                            <li className="navbar-li text-black text-left p-2 hover:text-white hover:underline">
-                                <Link className="navbar-link" to="/addrun">
-                                    Add Run
-                                </Link>
-                            </li>
-                            <li className="navbar-li text-black text-left p-2 hover:text-white hover:underline">
-                                <Link className="navbar-link" to="/myruns">
-                                    My Runs
-                                </Link>
-                            </li>
-                            <li className="navbar-li text-black text-left p-2 hover:text-white hover:underline">
-                                <Link className="navbar-link" to="/likes">
-                                    Likes
-                                </Link>
-                            </li>
-                        </ul>
+            <button className="ml-8 mr-16 sticky " onClick={toggleDropdown}>=</button>
+            <h2 className="mr-8 text-center">RUN, LOG, REPEAT</h2>
+            {isDropdownOpen &&
+                <ul className="navbar">
+                    <div className="text-left text-blue-600 mr-64 hover:underline" onClick={toggleDropdown}>RUNS
+                    {isDropdownOpen && 
+                        <div className="relative">
+                            <ul>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/home">
+                                        All Runs
+                                    </Link>
+                                </li>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/addrun">
+                                        Add Run
+                                    </Link>
+                                </li>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/myruns">
+                                        My Runs
+                                    </Link>
+                                </li>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/likes">
+                                        Likes
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    }
                     </div>
-                }
-                </li>
-                <li className="navbar-li text-right" onClick={toggleDropdown}>Profile
-                {isDropdownOpen && 
-                    <div className="relative">
-                        <ul>
-                            <li className="navbar-li text-black text-right p-2 hover:text-white hover:underline">
-                                <Link className="navbar-link" to={`/profile/${currentUser.id}`}>
-                                    My Profile
-                                </Link>
-                            </li>
-                            {localStorage.getItem("honey_user") ? (
-                            <li className="navbar-li text-white text-right p-2 hover:text-white hover:underline">
-                                <Link
-                                className="navbar-link"
-                                to=""
-                                onClick={() => {
-                                    localStorage.removeItem("honey_user")
-                                    navigate("/", { replace: true })
-                                }}
-                                >
-                                Logout
-                                </Link>
-                            </li>
-                            ) : (
-                            ""
-                            )}
-                        </ul>
+                    <div className="text-left mr-64 text-blue-600 hover:underline" onClick={toggleDropdown}>TRAINING PLANS
+                    {isDropdownOpen && 
+                        <div className="relative">
+                            <ul>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/addrun">
+                                        Add Run
+                                    </Link>
+                                </li>
+                                <li className="navbar-li text-black text-left p-2 hover:underline">
+                                    <Link className="navbar-link" to="/myruns">
+                                        My Runs
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    }
                     </div>
-                }</li>
-                
-                
-            </ul>
+                    <div className="text-right ml-64  text-blue-600 hover:underline" onClick={toggleDropdown}>PROFILE
+                    {isDropdownOpen && 
+                        <div className="relative">
+                            <ul>
+                                <li className="navbar-li text-black text-right p-2 hover:underline">
+                                    <Link className="navbar-link" to={`/profile/${currentUser.id}`}>
+                                        My Profile
+                                    </Link>
+                                </li>
+                                {localStorage.getItem("honey_user") ? (
+                                <li className="navbar-li text-black text-right p-2 underline">
+                                    <Link
+                                    className="navbar-link"
+                                    to=""
+                                    onClick={() => {
+                                        localStorage.removeItem("honey_user")
+                                        navigate("/", { replace: true })
+                                    }}
+                                    >
+                                    Logout
+                                    </Link>
+                                </li>
+                                ) : (
+                                ""
+                                )}
+                            </ul>
+                        </div>
+                    }
+                    </div>
+                </ul>
+            }
         </div>
     )
 }
