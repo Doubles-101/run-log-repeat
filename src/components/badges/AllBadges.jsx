6ec74f334a41d19ee2b19252436a9fa2d0870b49
William Woodard <williamwoodard98@gmail.com>
2024-05-10 15:55:14 -0500

    fully added dark mode

--- src/components/badges/AllBadges.jsx
+++ src/components/badges/AllBadges.jsx
@@ -2,11 +2,11 @@ import "./AllBadges.css"
 
 export const AllBadges = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <div className="color text-first p-8">
+        <div className="flex flex-col items-center justify-center h-screen dark:bg-fourth">
+            <div className="color text-first dark:text-first p-8">
                 <h1 className="text-6xl font-bold font-sans text-center">List of Badges</h1>
             </div>
-            <div className="max-w-4xl w-full bg-third p-6 rounded-lg shadow-lg flex">
+            <div className="max-w-4xl w-full bg-third dark:bg-first dark:text-fourth p-6 rounded-lg shadow-lg flex">
                 <ul>
                     <li>Complete Your First Run</li>
                     <li>3 Runs</li>
