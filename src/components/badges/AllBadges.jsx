567e9dead4380e49756f09c768b9f336c9aef681
William Woodard <williamwoodard98@gmail.com>
2024-05-10 16:19:52 -0500

    added badge icons

--- src/components/badges/AllBadges.jsx
+++ src/components/badges/AllBadges.jsx
@@ -1,4 +1,8 @@
 import "./AllBadges.css"
+import { TbRun } from "react-icons/tb"
+import { PiPersonSimpleRun, PiPersonSimpleRunBold, PiPersonSimpleRunDuotone, PiPersonSimpleRunFill, PiPersonSimpleRunLight, PiPersonSimpleRunThin } from "react-icons/pi"
+import { LiaRunningSolid } from "react-icons/lia"
+import { FaRunning } from "react-icons/fa"
 
 export const AllBadges = () => {
     return (
@@ -8,17 +12,17 @@ export const AllBadges = () => {
             </div>
             <div className="max-w-4xl w-full bg-third dark:bg-first dark:text-fourth p-6 rounded-lg shadow-lg flex">
                 <ul>
-                    <li>Complete Your First Run</li>
-                    <li>3 Runs</li>
-                    <li>10 Runs</li>
-                    <li>50 Runs</li>
-                    <li>100 Runs</li>
-                    <li>Like a Post</li>
-                    <li>Like 3 Posts</li>
-                    <li>Run 1 Mile</li>
-                    <li>Run 10 Miles</li>
-                    <li>Run 50 Miles</li>
-                    <li>Run 100 Miles</li>
+                    <li><div className="flex m-2"><TbRun className="mr-2"/>Complete Your First Run</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRun className="mr-2"/>3 Runs</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRunBold className="mr-2"/>10 Runs</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRunDuotone className="mr-2"/>50 Runs</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRunFill className="mr-2"/>100 Runs</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRun className="mr-2"/>Like a Post</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRun className="mr-2"/>Like 3 Posts</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRunLight className="mr-2"/>Run 1 Mile</div></li>
+                    <li><div className="flex m-2"><PiPersonSimpleRunThin className="mr-2"/>Run 10 Miles</div></li>
+                    <li><div className="flex m-2"><LiaRunningSolid className="mr-2"/>Run 50 Miles</div></li>
+                    <li><div className="flex m-2"><FaRunning className="mr-2"/>{"★"}Run 100 Miles</div></li>
                 </ul>
             </div>
         </div>
